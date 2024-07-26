c6d06c0fdebeed4b51376a81f70e0cdfd322ac5b
pomponiosimonegit config --global usern.email pomponio671@gmail.com <Pomponio671@gmail.com>
2024-07-26 15:44:03 +0200

    class video completo

--- src/Video.java
+++ src/Video.java
@@ -3,4 +3,33 @@ class Video extends ElementoMulti implements Riproduzione {
     private int volume;
     private int luminosita;
 
- 
+    public Video(String titolo, int durata, int volume, int luminosita) {
+        super(titolo);
+        this.durata = durata;
+        this.volume = volume;
+        this.luminosita = luminosita;
+    }
+
+    public void abbassaVolume() {
+        volume--;
+    }
+
+    public void alzaVolume() {
+        volume++;
+    }
+
+    public void aumentaLuminosita() {
+        luminosita++;
+    }
+
+    public void diminuisciLuminosita() {
+        luminosita--;
+    }
+
+    @Override
+    public void play() {
+        for (int i = 0; i < durata; i++) {
+            System.out.println(titolo + " " + "!".repeat(volume) + "*".repeat(luminosita));
+        }
+    }
+}
